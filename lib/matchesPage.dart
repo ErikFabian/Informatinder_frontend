66376a6f7666854c1a8b8df6efb3a13d50c5cacd
Nils Matthiesen <nilsmatthiesen@yahoo.de>
2022-04-08 15:00:08 +0200

    Add Matches Page UI without functionality

--- lib/matchesPage.dart
+++ lib/matchesPage.dart
@@ -7,11 +7,55 @@ class matchesPage extends StatelessWidget {
   final data;
   matchesPage({this.data});
 
+  Widget matchesSection(String profileName, String profile) {
+    return Column(children: [
+      Container(
+        margin: const EdgeInsets.only(left: 8, right: 8),
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          border: Border.all(width: 2, color: Colors.black),
+        ),
+        child: Row(
+          children: [
+            Expanded(
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      child: Text(
+                        profileName,
+                        style: const TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 30),
+                      ),
+                    ),
+                  ]),
+            ),
+            Expanded(
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('Profil'),
+                  )
+                ]))
+          ],
+        ),
+      )
+    ]);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("matchesPage"),
+        title: Text("Matches"),
+      ),
+      body: Column(
+        children: [
+          for (int i = 0; i <= 5; i++)
+            matchesSection("name" + i.toString(), ""),
+        ],
       ),
       bottomNavigationBar: bottomNavBar(),
     );
