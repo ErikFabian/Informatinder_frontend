e0455b909ae73196271352158c201ed7af9a604f
Nils Matthiesen <nilsmatthiesen@yahoo.de>
2022-05-15 21:28:54 +0200

    chnages things

--- lib/profilePage.dart
+++ lib/profilePage.dart
@@ -1,8 +1,47 @@
 import 'package:flutter/material.dart';
 
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
+
+import 'models/profile.dart';
+
+Future<Profile> getProfile(int id) async {
+  final response = await http.post(
+    Uri.parse('http://10.0.2.2:8080/profile/' + id.toString()),
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+  );
+
+  if (response.statusCode == 200) {
+    Map<String, dynamic> responseData = json.decode(response.body);
+    Profile profile = Profile.fromJson(responseData);
+    return profile;
+  } else {
+    throw Exception("No Profile found");
+  }
+}
+
 class profilePage extends StatelessWidget {
   @override
+  final int profileID;
+  profilePage({required Key key, required this.profileID}) : super(key: key);
   Widget build(BuildContext context) {
+    return FutureBuilder<Profile>(
+        future: getProfile(),
+        builder: (BuildContext context, AsyncSnapshot<Profile> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: Text('Loading'));
+          } else {
+            if (snapshot.hasError) {
+              return Center(child: Text('Error: ${snapshot.error}'));
+            } else {
+              Profile profile = snapshot.data!;
+              if (!editable) {
+                _profileNameController.text = profile.name;
+                _profileTextController.text = profile.description;
+              }
     return Scaffold(
         resizeToAvoidBottomInset: false,
         appBar: AppBar(title: const Text("InformaTinder")),
